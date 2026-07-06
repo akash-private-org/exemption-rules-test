eb54b716bc835e033451a238156c51b312b770c1
Akash Dhawale <235484553+cx-akash-dhawale@users.noreply.github.com>
2026-07-06 18:07:23 +0530

    Update sast-vulenrabilities.java

--- sast-vulenrabilities.java
+++ sast-vulenrabilities.java
@@ -1,6 +1,6 @@
 public class LoginValidator extends HttpServlet {
 
-//pr11
+//pr12
 protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 throws ServletException, IOException {
 
